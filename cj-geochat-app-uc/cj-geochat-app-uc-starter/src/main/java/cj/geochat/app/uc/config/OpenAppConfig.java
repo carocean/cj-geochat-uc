2962c6a08a70f9023b5709dceea29cba4d392476
cj <carocean.jofers@icloud.com>
2023-05-31 23:33:12 +0800

    1.0.0

--- cj-geochat-app-uc/cj-geochat-app-uc-starter/src/main/java/cj/geochat/app/uc/config/OpenAppConfig.java
+++ cj-geochat-app-uc/cj-geochat-app-uc-starter/src/main/java/cj/geochat/app/uc/config/OpenAppConfig.java
@@ -1,10 +1,9 @@
 package cj.geochat.app.uc.config;
 
 import cj.geochat.ability.oauth2.app.annotation.EnableCjOAuth2App;
-import cj.geochat.ability.oauth2.app.config.AppSecurityWorkbin;
 import org.springframework.context.annotation.Configuration;
 
 @EnableCjOAuth2App
 @Configuration
-public class OpenAppConfig extends AppSecurityWorkbin {
+public class OpenAppConfig {
 }
